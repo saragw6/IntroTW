a9ad11989082e9f57c1e320257e188c45e884b4a
Sara <saragw6@gmail.com>
2018-06-27 10:51:22 -0400

    single, horiz, and vertical

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,37 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println('*');
+        Scanner in = new Scanner(System.in);
+
+        single();
+
+        horizontal(in);
+
+        vertical(in);
+    }
+
+    private static void single() {
+        System.out.println('*');
+    }
+
+    private static void vertical(Scanner in) {
+        System.out.println("Enter n for vertical line:");
+        Integer vertical = Integer.parseInt(in.nextLine());
+        for (int i = 0; i < vertical; i++) {
+            single();
+        }
+        System.out.println();
+    }
+
+    private static void horizontal(Scanner in) {
+        System.out.println("Enter n for horizontal line:");
+        Integer horizontal = Integer.parseInt(in.nextLine());
+        for (int i = 0; i < horizontal; i++) {
+            System.out.print('*');
+        }
+        System.out.println();
     }
 }
